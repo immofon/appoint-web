579c2ffca6d7c67edc53d41bcce57bb3ab5f9a14
mofon <yzkf000@foxmail.com>
2018-11-18 13:12:49 +0800

    rm About

--- src/router.js
+++ src/router.js
@@ -44,13 +44,6 @@ export default new Router({
       path: '/error',
       props: true,
       component: ErrorPage,
-    }, {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import( /* webpackChunkName: "about" */ './views/About.vue')
     }
   ]
 })
